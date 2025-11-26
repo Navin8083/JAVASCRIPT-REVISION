dab6706a6057391b5aeb4236c87384bde464b8b1
NavinKumar <150262611+Navin8083@users.noreply.github.com>
2025-11-26 14:26:53 +0000

    Conversion

---
+++ 01_basics/03_Conversion.js
@@ -0,0 +1,18 @@
+let isLogIn="Naveen"
+let booleanislogIn=Boolean(isLogIn);
+
+// console.log(booleanislogIn); -> true
+
+let somenumber=33
+//let rank=null
+let score="44abc" // --> NaN (Not a number)
+// "33"-> 33
+
+let Stringnumber= String(somenumber)
+console.log( typeof Stringnumber) // --> String
+//console.log(typeof score) // object
+//console.log(typeof score)
+
+let valueInScore=Number(score)
+console.log( valueInScore)
+
