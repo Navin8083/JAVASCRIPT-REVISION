d7fd867bc1b030e040a23c8e1ae2435bc43d8b9a
NavinKumar <150262611+Navin8083@users.noreply.github.com>
2025-11-26 15:23:38 +0000

    OperationsConversion

---
+++ 01_basics/04_ConversionOperations.js
@@ -0,0 +1,27 @@
+let str1="Hello"
+let str2=" Naveen"
+let str3= str1+str2;
+//console.log(str3); Hello Naveen
+
+let value =3;
+let negvalue=-value;
+//console.log(negvalue) -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2==2); 
+
+// console.log("1"+2)  // 12
+// console.log(1+"2")  // 12
+// console.log("1"+2 + 2) // 122
+// console.log(1+ 2 +"2")  //32
+
+console.log(2001 + ": Its my birthday")
+
+
+
+
