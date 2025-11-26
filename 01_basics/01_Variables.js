e8f9c5921dd523b6de249042091826c6684d9053
NavinKumar <150262611+Navin8083@users.noreply.github.com>
2025-11-26 13:24:50 +0000

    variable

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId=10234
+let name="Naveen"
+var email="naveen@gmail.com"
+customerNumber="33949434345"
+
+// accountId=9210 Not allowed
+name="NaveenKumar"
+email="22010102@cutm.ac.in"
+customerNumber="34567676456"
+
+console.log(name);
+console.table([accountId,name,email,customerNumber])
