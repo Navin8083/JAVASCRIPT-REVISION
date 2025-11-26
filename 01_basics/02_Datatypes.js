fa761dfb83a8c5413540a311438d2287c6d3ebda
NavinKumar <150262611+Navin8083@users.noreply.github.com>
2025-11-26 13:45:53 +0000

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,20 @@
+"use strict"
+
+// alert(3+3) // we are using nodejs, not browser
+console.log(3+3);
+
+console.log("Naveen");
+let name="Naveen"           // Strings
+let age=19                 // integer(number)
+let isLoggedIn = false;    // Boolean 
+let state=null;           // null standalone value
+ 
+// symbol = unique
+//object
+
+console.log(typeof age)        // number
+console.log(typeof state)       //object 
+console.log(typeof undefined)    // undefined
+console.log(typeof null)        // object
+
+
