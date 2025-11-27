d86309e252b17205763c6f0f1b20544f82422c79
NavinKumar <150262611+Navin8083@users.noreply.github.com>
2025-11-27 06:16:59 +0000

    DatatypeSummery

--- 01_basics/04_ConversionOperations.js
+++ 01_basics/04_ConversionOperations.js
@@ -20,7 +20,44 @@ let negvalue=-value;
 // console.log("1"+2 + 2) // 122
 // console.log(1+ 2 +"2")  //32
 
-console.log(2001 + ": Its my birthday")
+//console.log(2001 + ": Its my birthday")
+
+// console.log("2"==2)  true
+// console.log("2"===2) false 
+// console.log(null>0)  false 
+// console.log(null>=0) true
+
+// Datatypes summery
+
+// premitive 
+// 7types-: String ,Number, Boolean , null, undefined ,symbol,BigInt
+const score=100
+const scoreValue=100
+
+//console.log(score === scoreValue);
+
+const isloggin=false;  // Boolean
+const outerTemp=null; // object
+let userEmail; // undefined
+
+const id= Symbol('123')
+const anotherId= Symbol('123')
+
+//console.log(id===anotherId);  -->false
+
+// Refrence (Non primitive)
+
+// Array, Object , Functions
+const heros=["Naveen","Chandan","naggraj"]
+let myObj={
+     name: "naveen",
+    age:22,
+}
+const myFunction=function(){
+console.log("Hello World");
+
+}
+console.log(typeof userEmail)
 
 
 
