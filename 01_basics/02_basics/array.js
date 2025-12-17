4095415c6338256b8399cd020e4e2e38f43cb6f8
NavinKumar <150262611+Navin8083@users.noreply.github.com>
2025-12-17 07:54:31 +0000

    number and array

---
+++ 01_basics/02_basics/array.js
@@ -0,0 +1,59 @@
+const arr=[1,2,3,4,5]
+//console.log(arr);
+
+
+//arr.push(9)
+// arr.pop()
+// console.log(arr);
+
+
+// arr.shift()
+// console.log(arr);      // ---> 2,3,4,5
+
+// arr.unshift(8)
+// console.log(arr);     // ---> 8,1,2,3,4,5
+
+// console.log(arr.includes(7)); -->false
+// console.log(arr.indexOf(4));
+
+// const newarr=arr.join()
+// console.log(arr)           --->[ 1, 2, 3, 4, 5 ]
+// console.log(newarr);           1,2,3,4,5   
+// console.log(typeof newarr)    string
+
+// const newarr1 = arr.slice(1,3)
+// console.log(newarr1);           [2,3]
+// console.log("A", arr)         same as original array  A [ 1, 2, 3, 4, 5 ]
+
+// const newarr2=arr.splice(1,3)
+// console.log(newarr2);         //  [2,3,4]
+// console.log("B", arr)       // --->original array change  B [ 1, 5 ]
+
+
+// part 2 array
+
+const marvel=["Naveen","Rahul","Raju","Rakesh" ];
+const dc=["Aftab","sahil","Awesh"]
+
+//     marvel.push(dc)
+// console.log(marvel);
+// const all_heros=marvel.concat(dc)
+// console.log(all_heros);
+
+//console.log(...marvel,...dc)
+
+// const number=[1,3,4,2,5,[5,6,7,[4,5]]]
+// const all_number=number.flat(Infinity)
+// console.log(all_number);
+  let a=12;
+  let b=13;
+  let c=15;
+
+  console.log(Array.of(a,b,c))
+
+
+
+
+
+
+
---
+++ 01_basics/06_Numbers_math.js
@@ -0,0 +1,25 @@
+const number=400;
+//console.log(number);
+//const mynumber= new Number(100);
+//console.log(mynumber.toString().length);
+
+// console.log(number.toFixed(2));
+
+const newnumber=13.956
+//console.log(newnumber.toPrecision(3))
+
+const bigNumber=1000000
+//console.log(bigNumber.toLocaleString());
+
+//console.log(bigNumber.toLocaleString('en-IN'));
+
+//***************math*****************/
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(6.8))
+// console.log(Math.sqrt(25))
+console.log(Math.random())
+console.log((Math.random()*10+1));
+
+console.log(Math)
